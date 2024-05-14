91f3bdef2b0bb9d17d6b8a5051bc30dcaf7812b2
itc-s23024 <s23024@std.it-college.ac.jp>
2024-05-14 17:00:36 +0900

    5/14 16~18章課題5更新終わり

--- src/main/kotlin/Check05.kt
+++ src/main/kotlin/Check05.kt
@@ -1,37 +1,46 @@
 package org.example
 
 import kotlin.random.Random
+import kotlin.random.nextInt
 
 fun main(args: Array<String>) {
-    //Check03 で作ったプログラムが、本当に確率通りか試してみる
-    println("試行回数?:")
+    // Check03 で作ったプログラムが、本当に確率通りか試してみる
+    print("試行回数?: ")
     val count = readln().toInt()
 
-    //かち
-//勝ちが1割,負け8.5割,あいこ0.5割となる
-// CPUを実装したじゃんけんを作って下さい
-    println("1:グー 2:チョキ 3:パー を選んでください: ")
-    //0,1,2でグー、チョキ、パー選択する
-    val player = readln().toInt() - 1
+    // かち・まけ・あいこ の数を数える変数を用意する
+    var win = 0
+    var lose = 0
+    var draw = 0
 
-    // 乱数で勝敗を決める
-    val result = when (Random.nextInt(0,100)) {
-        in 0 .. 10 -> 1  //勝ち
-        in 10 .. 15 -> 0 //負け
-        else -> 2  //あいこ
+    for (i in 0..<count) {
+        val player = Random.nextInt(1..3)
+        println("1:グー 2:チョキ 3:パー を選びました: $player")
+
+        // 乱数で勝敗を決める
+        val (result, judge) = when (Random.nextInt(0..<100)) {
+            in 0..<10 -> {
+                win++
+                Pair(1, "かち")
+            }
+            in 10..<15 -> {
+                draw++
+                Pair(0, "あいこ")
+            }
+            else -> {
+                lose++
+                Pair(2, "まけ")
+            }
+        }
+        // CPUの手を計算で求める
+        val cpu = (player + result) % 3
+
+        // 結果表示
+        // 勝敗の結果表示(プレイヤー視点)
+        println("CPU: ${cpu + 1} --- $judge")
     }
-
-    //CPUの手を計算で求める
-    val cpu = (player + result) % 3
-
-    //表示用の勝ち負けあいこ
-    val judje = when(result) {
-        0 -> "あいこ"
-        1 -> "勝ち"
-        else -> "負け"
-    }
-
-    //結果表示
-    //勝敗の結果表示（プレイヤー視点）
-    println("CPU: ${cpu + 1} --- $judje")
+    println("確率の結果")
+    println("かち: $win (${win / count.toDouble() * 100}%)")
+    println("まけ: $lose (${lose / count.toDouble() * 100}%)")
+    println("あいこ: $draw (${draw / count.toDouble() * 100}%)")
 }
