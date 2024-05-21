b407f4546243c300470c68d70c5f2ecc56fa4186
itc-s23024 <s23024@std.it-college.ac.jp>
2024-05-21 09:18:58 +0900

    5/15 18~22章更新終わり

--- src/main/kotlin/Chap18.kt
+++ src/main/kotlin/Chap18.kt
@@ -1,5 +1,6 @@
 package org.example
 
+import kotlin.random.Random
 import kotlin.time.measureTime
 
 fun main(args: Array<String>) {
@@ -28,22 +29,55 @@ fun main(args: Array<String>) {
         println(s)
     }
 
-    val charList = 'a'..'z'
-    val searchCharList = mutableListOf('z', 'x', 'c', 'v', 'b', 'n', 'm')
-    val searchCharSet = mutableSetOf('z', 'x', 'c', 'v', 'b', 'n', 'm')
-    val count = 1_000_000_000L
+//    val charList = 'a'..'z'
+//    val searchCharList = mutableListOf('z', 'x', 'c', 'v', 'b', 'n', 'm')
+//    val searchCharSet = mutableSetOf('z', 'x', 'c', 'v', 'b', 'n', 'm')
+//    val count = 1_000_000_000L
+//    val listTime = measureTime {
+//        for (i in 0..<count) {
+//            val target = charList.shuffled()[0]
+//            println("「${target}」は存在する？ : ${target in searchCharList}")
+//        }
+//    }
+//    val setTime = measureTime {
+//        for (i in 0..count) {
+//            val target = charList.shuffled()[0]
+//            println("「${target}」は存在する？ : ${target in searchCharList}")
+//        }
+//    }
+//    println("Listでかかった時間: $listTime")
+//    println(" setでかかった時間: $setTime" )
+
+    println("----ListとSetの比較----")
+    val dataSet = mutableSetOf<Int>()
+    while (dataSet.count() < 10000) {
+        dataSet += Random.nextInt()
+    }
+    val dataList = dataSet.toMutableList()
+
+    val searchList = mutableListOf<Int>()
+    for (i in 0..100_000) {
+        searchList += Random.nextInt()
+    }
+    val repeat = 500
+
     val listTime = measureTime {
-        for (i in 0..<count) {
-            val target = charList.shuffled()[0]
-            println("「${target}」は存在する？ : ${target in searchCharList}")
+        for (r in 0..<repeat) {
+            println("List ${r}セット目")
+            for (s in searchList) {
+                dataList.contains(s)
+            }
         }
     }
+
     val setTime = measureTime {
-        for (i in 0..count) {
-            val target = charList.shuffled()[0]
-            println("「${target}」は存在する？ : ${target in searchCharList}")
+        for (r in 0..<repeat) {
+            println("Set ${r}セット目")
+            for (s in searchList) {
+                dataSet.contains(s)
+            }
         }
     }
-    println("Listでかかった時間: $listTime")
-    println(" setでかかった時間: $setTime" )
+    println("Listの所要時間: $listTime")
+    println(" Setの所要時間: $setTime")
 }
